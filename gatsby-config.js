e76de43d71668e9b7dc6d60d23eb2f8d51b36c37
Damien R <damien.rembert@gmail.com>
2024-06-10 19:23:06 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ltl-usage-graph`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
