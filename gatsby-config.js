d522717442c8be5f8251912b1fe1cd8b39a886ff
Damien R <damien.rembert@gmail.com>
2024-06-13 00:20:03 +0100

    enable dotenv and csv usage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,27 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `ltl-usage-graph`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  pathPrefix: "/map-turner",
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `data`,
+        // Path to the directory
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    `gatsby-transformer-csv`,
+  ],
+};
