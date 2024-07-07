c0550c984c9cad476382cc1e12ff4ab686f5fcbb
Damien R <damien.rembert@gmail.com>
2024-07-07 23:19:04 +0100

    display all users - Indicate the sum of all users when displaying the number of new users #15

--- src/pages/first.js
+++ src/pages/first.js
@@ -1,65 +1,71 @@
 import React from "react";
 import { graphql } from "gatsby";
 import {
-	Chart as ChartJS,
-	CategoryScale,
-	LinearScale,
-	PointElement,
-	LineElement,
-	Title,
-	Tooltip,
-	Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
 
 ChartJS.register(
-	CategoryScale,
-	LinearScale,
-	PointElement,
-	LineElement,
-	Title,
-	Tooltip,
-	Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 const FirstsPage = ({ data }) => {
-	let labels = [];
-	let firsts = [];
+  let labels = [];
+  let firsts = [];
+  let allUsers = [];
 
+  data.allFirstCsv.edges.forEach((item) => {
+    allUsers.push(item.node.all_users);
+    firsts.push(item.node.New_Users);
+    labels.push(item.node.Start_Date + " to " + item.node.End_Date);
+  });
 
-	data.allFirstCsv.edges.forEach((item) => {
-		firsts.push(item.node.New_Users);
-		labels.push(item.node.Start_Date + " to " + item.node.End_Date);
-	});
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "New Users",
+      },
+    },
+  };
 
-	const options = {
-		responsive: true,
-		plugins: {
-			legend: {
-				position: "top",
-			},
-			title: {
-				display: true,
-				text: "New Users",
-			},
-		},
-	};
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        label: "First Time Users",
+        data: firsts,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "All Users",
+        data: allUsers,
+        borderColor: "rgb(255, 161, 99)",
+        backgroundColor: "rgba(255, 161, 99, 0.5)",
+      },
+    ],
+  };
 
-	const chartData = {
-		labels,
-		datasets: [
-			{
-				label: "New_Users",
-				data: firsts,
-				borderColor: "rgb(255, 99, 132)",
-				backgroundColor: "rgba(255, 99, 132, 0.5)",
-			},
-		],
-	};
-
-	return <Line options={options} data={chartData} />;
+  return <Line options={options} data={chartData} />;
 };
-
 
 export const query = graphql`
   query MyQuery {
@@ -69,6 +75,7 @@ export const query = graphql`
           Start_Date
           End_Date
           New_Users
+          all_users
         }
       }
     }
