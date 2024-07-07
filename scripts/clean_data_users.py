eac7a6ab022ed21ef59285679394efe05e353b07
Damien R <damien.rembert@gmail.com>
2024-07-07 23:17:12 +0100

    get total users

--- scripts/clean_data_users.py
+++ scripts/clean_data_users.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
 
-def clean_data(df):
-    df = df.drop(columns=["Count", "Amount"])
-
-    df = df.rename(columns={"Value": "User"})
+def get_first_time_users_per_week(df):
 
     # Only keep users' first appearance
     df = (
@@ -30,6 +27,15 @@ def clean_data(df):
     return df
 
 
+def get_total_users_per_week(df):
+    df = (
+        df.groupby(["Start_Date", "End_Date"])
+        .agg(all_users=("User", "count"))
+        .reset_index()
+    )
+    return df
+
+
 def get_dates_from_filename(df):
     # Replace all instances of "data/user/|\\.csv" with "" in column: 'Filename'
     df["Filename"] = df["Filename"].str.replace(
@@ -45,9 +51,28 @@ def get_dates_from_filename(df):
     return df
 
 
+def prepare_data(df):
+    df = df.drop(columns=["Count", "Amount"])
+
+    df = df.rename(columns={"Value": "User"})
+    return df
+
+
+def clean_data(df):
+    df = df.rename(columns={"End_Date_y": "End_Date"})
+    df = df.drop(columns=["End_Date_x"])
+    df["New_Users"] = df["New_Users"].fillna(0).astype(int)
+    return df
+
+
 df = pd.read_csv(r"data/user_collection.csv", engine="pyarrow")
 df = get_dates_from_filename(df)
+df = prepare_data(df)
 
+df_total_users = get_total_users_per_week(df.copy())
+df_first_time_users = get_first_time_users_per_week(df.copy())
 
-df_clean = clean_data(df.copy())
-df_clean.to_csv(r"src/data/first.csv", index=False)
+df = pd.merge(df_first_time_users, df_total_users, how="outer", on="Start_Date")
+df = clean_data(df)
+
+df.to_csv(r"src/data/first.csv", index=False)
